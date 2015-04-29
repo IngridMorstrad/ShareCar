1fe6b7bbcc044108f1a4f5fc809db0891c34023c
Ashwin Menon <ashwin91+github@gmail.com>
2015-04-29 18:30:55 -0500

    Login error display
    Failed login displays an error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	if @user
   		flash[:notice] = "You've been logged in"
   		session[:user_id] = @user.id
-  		redirect_to "/"
+  		redirect_to root_path
   	else
   		flash[:alert] = "There was a problem logging you in"
   		redirect_to log_in_path
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
   def destroy
   	session[:user_id] = nil
   	flash[:notice] = "You've been logged out successfully"
-  	redirect_to "/"
+  	redirect_to log_in_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,22 @@
 class UsersController < ApplicationController
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
-  	@user = User.new(user_create_params)
-  	if @user.save
+    @user = User.new(user_create_params)
+    if @user.save
       flash[:notice] = "Successfully created account!"
       session[:user_id] = @user.id
       redirect_to "/"
-  	else
+    else
       flash[:alert] = "There was a problem in creating your account! Please, Try again."
-  		render 'new'
-  	end
+      render 'new'
+    end
   end
 
   private
-
   def user_create_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password)
   end
 end
