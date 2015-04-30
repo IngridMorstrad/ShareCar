1ffd8b6af3d971573c05296948e504399d4d85b2
Ashwin Menon <ashwin91+github@gmail.com>
2015-04-29 19:49:11 -0500

    Tests for valid trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ActiveRecord::Base
     has_one :car
     has_many :passenger
+    validates :distance, presence: true, numericality: {greater_than: 0}
+    validates :origin, :destination, :start_time, :end_time, presence: true
 end
--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class TripTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @trip = Trip.create(distance: 8, car_id: 1, origin: 'High point woods', destination: 'Epic', new_passenger_cost: 1.1/2*0.945, start_time: DateTime.new(2015,4,1,17), end_time: DateTime.new(2015,4,1,17,30), total_trip_cost: 0.945, completed: false)
+  end
+
+  test "should be valid" do
+    assert @trip.valid?
+  end
+
+  test "distance should be a number" do
+    @trip.distance = "invalid"
+    assert_not @trip.valid?
+  end
+
+  test "origin should be present" do
+    @trip.origin = " "
+    assert_not @trip.valid?
+  end
+
+  test "destination should be present" do
+    @trip.destination = " "
+    assert_not @trip.valid?
+  end
+
+  test "start time should be present" do
+    @trip.start_time = ""
+    assert_not @trip.valid?
+  end
+
+  test "end time should be present" do
+    @trip.end_time = ""
+    assert_not @trip.valid?
+  end
+
 end
