c95fddea9ce9035d5486fafc1264b79d572d3d2d
Ashwin Menon <ashwin91+github@gmail.com>
2015-07-08 22:16:41 -0500

    Changed some tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,21 +3,17 @@ require 'test_helper'
 class SessionsControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
-    assert_response :success
+    assert_redirected_to log_in_path
   end
 
   test "should get create" do
     get :create
-    assert_response :success
+    assert_redirected_to log_in_path
   end
 
   test "should get destroy" do
-    user=User.new(id:1,name:"venky",email:"blah@gmail.com",password:"papajohns",password_confirmation:"papajohns")
-    if user.save
-      cookies[:auth_token]=user.auth_token
-    else
-      puts "WTF BRO"
-    end
+    user=User.create(name:"venky",email:"blah@gmail.com",password:"papajohns",password_confirmation:"papajohns",activated:true)
+    cookies[:auth_token]=user.auth_token
     get :destroy, :id => user.id
     assert_redirected_to(controller: "main")
   end
