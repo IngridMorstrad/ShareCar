b989d711f0757cf3ccbda244d807531c1d2e0399
Ashwin Menon <ashwin91+github@gmail.com>
2015-07-24 13:58:59 -0500

    Renamed "ShareCar" to "Trippa"
    Also added runserver.bat

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719124157) do
+ActiveRecord::Schema.define(version: 20150724065741) do
 
   create_table "cars", force: true do |t|
     t.string   "make"
